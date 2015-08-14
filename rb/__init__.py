9284598af5de1ca3d786db62011a8fd9ca1998e0
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-14 21:51:13 +0200

    Moved some imports to the package.

--- rb/__init__.py
+++ rb/__init__.py
@@ -7,3 +7,14 @@
     :copyright: (c) 2015 Functional Software Inc.
     :license: Apache License 2.0, see LICENSE for more details.
 """
+from rb.cluster import Cluster
+from rb.router import BaseRouter, ConsistentHashingRouter, PartitionRouter
+
+
+__all__ = [
+    # cluster
+    'Cluster',
+
+    # router
+    'BaseRouter', 'ConsistentHashingRouter', 'PartitionRouter',
+]
