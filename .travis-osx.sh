2d8eb87e52c4350b8006652c343937c73235bccd
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-19 15:03:09 +0200

    Made travis for OS X work

---
+++ .travis-osx.sh
@@ -0,0 +1,8 @@
+# Install packages with brew
+brew update >/dev/null
+brew outdated pyenv || brew upgrade --quiet pyenv
+
+# Install required python version for this build
+pyenv install -ks $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+python --version
