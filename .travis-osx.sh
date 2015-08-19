641fef09c7ffea8f545cb180a0f42c36d0432847
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-19 18:37:14 +0200

    Install redis on OSX

--- .travis-osx.sh
+++ .travis-osx.sh
@@ -1,6 +1,7 @@
 # Install packages with brew
 brew update >/dev/null
 brew outdated pyenv || brew upgrade --quiet pyenv
+brew outdated redis || brew install --quiet redis
 
 # Install required python version for this build
 pyenv install -ks $PYTHON_VERSION
