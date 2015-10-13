89dcd6adbeaffe44044bbdfba6afcc3b6d7ea10e
Shipeng Feng <fsp261@gmail.com>
2015-10-13 18:38:07 +0800

    Do not pass cluster to LocalClient

--- rb/clients.py
+++ rb/clients.py
@@ -545,11 +545,10 @@ class LocalClient(BaseClient):
     host.
     """
 
-    def __init__(self, cluster, connection_pool=None, **kwargs):
+    def __init__(self, connection_pool=None, **kwargs):
         if connection_pool is None:
             raise TypeError('The local client needs a connection pool')
-        BaseClient.__init__(self, cluster, connection_pool=connection_pool,
-                            **kwargs)
+        BaseClient.__init__(self, connection_pool=connection_pool, **kwargs)
 
 
 class MapManager(object):
