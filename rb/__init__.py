8d94bcbec03d88d5023cf6e871afa448673956e6
Armin Ronacher <armin.ronacher@active-4.com>
2017-06-23 00:15:06 +0200

    1.7

--- rb/__init__.py
+++ rb/__init__.py
@@ -14,7 +14,7 @@ from rb.router import BaseRouter, ConsistentHashingRouter, PartitionRouter, \
 from rb.promise import Promise
 
 
-__version__ = '1.6'
+__version__ = '1.7'
 
 __all__ = [
     # cluster
