f4c50bc1a486df1c3abca52cd157c86763152007
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-08-11 15:54:02 -0700

    fix(py3): Pin redis to known good client range (#30)
    This will also drop 2.6

--- setup.py
+++ setup.py
@@ -3,27 +3,26 @@ import ast
 from setuptools import setup
 
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s+=\s+(.*)")
 
 
-with open('rb/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open("rb/__init__.py", "rb") as f:
+    version = str(
+        ast.literal_eval(_version_re.search(f.read().decode("utf-8")).group(1))
+    )
 
 
 setup(
-    name='rb',
-    author='Functional Software Inc.',
-    author_email='hello@getsentry.com',
+    name="rb",
+    author="Functional Software Inc.",
+    author_email="hello@getsentry.com",
     version=version,
-    url='http://github.com/getsentry/rb',
-    packages=['rb'],
-    description='rb, the redis blaster',
-    install_requires=[
-        'redis>=2.6',
-    ],
+    url="http://github.com/getsentry/rb",
+    packages=["rb"],
+    description="rb, the redis blaster",
+    install_requires=["redis>=2.6,<2.11"],
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
     ],
 )
